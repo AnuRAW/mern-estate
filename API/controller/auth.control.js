757bcd0e53f9ae40617087197510150bf3d9411b
AnuRAW <anuragrawat230@gmail.com>
2024-04-07 12:33:24 +0530

    commit

--- API/controller/auth.control.js
+++ API/controller/auth.control.js
@@ -1,15 +1,14 @@
-import User from "../model/user.model.js";
-import bcrypt from  'bcrypt';
+import User from '../model/user.model.js';
+import bcrypt from 'bcrypt';
 
-export const signup = async (req, res,next) => {
-    const { username, email, password } = req.body;
-    const hashedPassword =  await bcrypt.hash(password,  10);
-    try{
-    const newUser = new User({ username, email, password :  hashedPassword });
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrypt.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
     await newUser.save();
-    res.status(201).json("User has been created sucessfully");
-    }catch(error){
-        next(error)
-        }
-    }
-
+    res.status(201).json('User created successfully!');
+  } catch (error) {
+    next(error);
+  }
+};
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import { Link } from "react-router-dom"
 
 function SignUp() {
+  const [formData,setFormData] = useState({})
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    console.log(formData)
+    const res = await fetch('/api/auth/signup',
+    {
+      method: 'POST',
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body: JSON.stringify(formData)
+    });
+    const data =await res.json();
+    console.log(data)
+   }
+   console.log(formData)
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-center text-3xl font-semibold my-7 '>Sign Up</h1>
-      <form className="flex flex-col gap-y-5">
-        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username' />
-        <input type="email" placeholder='Email' className='border p-3 rounded-lg ' id='email' />
-        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' />
+      <form className="flex flex-col gap-y-5" onSubmit={handleSubmit}>
+        <input type="text" placeholder='Username' className='border p-3 rounded-lg' id='username' onChange={handleChange} />
+        <input type="email" placeholder='Email' className='border p-3 rounded-lg ' id='email' onChange={handleChange}/>
+        <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password'onChange={handleChange} />
         <button type='submit' className='bg-blue-600 hover:bg-blue-800 transition duration-200 text-white font-bold py-2 border rounded-lg uppercase'>Signup</button>
       </form>
       <div className="flex gap-2 mt-5">
